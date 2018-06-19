3774b76ed170e2fd24f7bca76db0969500642f67
AdsCryptoz22 <40346381+AdsCryptoz22@users.noreply.github.com>
2018-06-19 06:08:52 +0200

    Add files via upload

---
+++ Add-Ons/AddonAreaOfEffect.cs
@@ -0,0 +1,135 @@
+﻿using EasyBuildSystem.Runtimes.Internal.Addons;
+using EasyBuildSystem.Runtimes.Internal.Builder;
+using EasyBuildSystem.Runtimes.Events;
+using UnityEngine;
+using EasyBuildSystem.Runtimes.Internal.Area;
+using EasyBuildSystem.Runtimes.Internal.Socket;
+using EasyBuildSystem.Runtimes.Internal.Managers;
+
+[AddOn(ADDON_NAME, ADDON_AUTHOR, ADDON_DESCRIPTION, AddOnTarget.BaseBuilder)]
+public class AddonAreaOfEffect : AddOnBehaviour
+{
+    #region AddOn Fields
+
+    public const string ADDON_NAME = "Add-On Area Of Effect";
+    public const string ADDON_AUTHOR = "Ads Cryptoz 22";
+    public const string ADDON_DESCRIPTION = "Disable all the sockets/areas if out of range, this allow to improve the performances of your scene.";
+
+    [HideInInspector]
+    public string _Name = ADDON_NAME;
+
+    public override string Name
+    {
+        get
+        {
+            return _Name;
+        }
+
+        protected set
+        {
+            _Name = value;
+        }
+    }
+
+    [HideInInspector]
+    public string _Author = ADDON_AUTHOR;
+
+    public override string Author
+    {
+        get
+        {
+            return _Author;
+        }
+
+        protected set
+        {
+            _Author = value;
+        }
+    }
+
+    [HideInInspector]
+    public string _Description = ADDON_DESCRIPTION;
+
+    public override string Description
+    {
+        get
+        {
+            return _Description;
+        }
+
+        protected set
+        {
+            _Description = value;
+        }
+    }
+
+    #endregion AddOn Fields
+
+    #region Public Fields
+
+    [Tooltip("This allows to define the radius area effect.")]
+    public float Radius = 30f;
+
+    [Tooltip("This allows to define the refresh interval (Default:0.5f).")]
+    public float RefreshInterval = 0.5f;
+
+    #endregion
+
+    #region Private Methods
+
+    private void OnEnable()
+    {
+        EventHandlers.OnBuildModeChanged += OnModeChanged;
+    }
+
+    private void OnDisable()
+    {
+        EventHandlers.OnBuildModeChanged -= OnModeChanged;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        //Show the aoe radius when the component is selected.
+        Gizmos.DrawWireSphere(transform.position, Radius);
+    }
+
+    private void OnModeChanged(BuildMode mode)
+    {        
+        //We make sure that the builder behaviour instance is not null.
+        if (BuilderBehaviour.Instance == null)
+            return;
+
+        //If the current mode is None then we disable all the sockets also that the areas.
+        if (BuilderBehaviour.Instance.CurrentMode == BuildMode.None)
+        {
+            foreach (AreaBehaviour Area in BuildManager.Instance.Areas)
+                Area.gameObject.SetActive(false);
+
+            foreach (SocketBehaviour Socket in BuildManager.Instance.Sockets)
+                if (Socket != null)
+                    Socket.DisableCollider();
+        }
+        //If the current mode is Placement/Edition then we active only the socket of same type that the current selected preview type.
+        else if (BuilderBehaviour.Instance.CurrentMode == BuildMode.Placement || BuilderBehaviour.Instance.CurrentMode == BuildMode.Edition)
+        {
+            foreach (AreaBehaviour Area in BuildManager.Instance.Areas)
+                Area.gameObject.SetActive((Vector3.Distance(transform.position, Area.transform.position) <= Radius));
+
+            foreach (SocketBehaviour Socket in BuildManager.Instance.Sockets)
+            {
+                if (Socket != null)
+                {
+                    if (Vector3.Distance(transform.position, Socket.transform.position) <= Radius)
+                    {
+                        if (Socket.AttachedPart != null)
+                            Socket.EnableColliderByType(BuilderBehaviour.Instance.SelectedPrefab.Type);
+                    }
+                    else
+                        Socket.DisableCollider();
+                }
+            }
+        }
+    }
+
+    #endregion
+}
---
+++ Add-Ons/AddonDetachChildrens.cs
@@ -0,0 +1,165 @@
+﻿using EasyBuildSystem.Runtimes;
+using EasyBuildSystem.Runtimes.Internal.Addons;
+using EasyBuildSystem.Runtimes.Internal.Builder;
+using EasyBuildSystem.Runtimes.Internal.Part;
+using EasyBuildSystem.Runtimes.Events;
+using EasyBuildSystem.Runtimes.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[AddOn(ADDON_NAME, ADDON_AUTHOR, ADDON_DESCRIPTION, AddOnTarget.BasePart)]
+public class AddonDetachChildrens : AddOnBehaviour
+{
+    #region AddOn Fields
+
+    public const string ADDON_NAME = "Add-On Detach Childrens";
+    public const string ADDON_AUTHOR = "R.Andrew";
+    public const string ADDON_DESCRIPTION = "Spawn gameObject(s) when the placement/destruction/edition is performed.";
+
+    [HideInInspector]
+    public string _Name = ADDON_NAME;
+
+    public override string Name
+    {
+        get
+        {
+            return _Name;
+        }
+
+        protected set
+        {
+            _Name = value;
+        }
+    }
+
+    [HideInInspector]
+    public string _Author = ADDON_AUTHOR;
+
+    public override string Author
+    {
+        get
+        {
+            return _Author;
+        }
+
+        protected set
+        {
+            _Author = value;
+        }
+    }
+
+    [HideInInspector]
+    public string _Description = ADDON_DESCRIPTION;
+
+    public override string Description
+    {
+        get
+        {
+            return _Description;
+        }
+
+        protected set
+        {
+            _Description = value;
+        }
+    }
+
+    #endregion AddOn Fields
+
+    #region Public Fields
+
+    [Tooltip("This allows to add a rigidobdy to each childs when destroyed.")]
+    public bool AddDynamicRigidbody;
+
+    [Tooltip("This allows to define the max depenetration velocity when destroyed.")]
+    public float MaxDepenetrationVelocity = 2f;
+
+    [Tooltip("This allows to add a box collider to each childs when destroyed.")]
+    public bool AddDynamicBoxCollider;
+
+    public float LifeTime = 10f;
+
+    [HideInInspector]
+    public PartBehaviour Part;
+
+    #endregion
+
+    #region Private Methods
+
+    private void OnEnable()
+    {
+        EventHandlers.OnDestroyedPart += OnDestroyedPart;
+    }
+
+    private void OnDisable()
+    {
+        EventHandlers.OnDestroyedPart -= OnDestroyedPart;
+    }
+    
+    private void Awake()
+    {
+        //Get the Base Part component in this object.
+        Part = GetComponent<PartBehaviour>();
+    }
+
+    private bool IsExiting;
+
+    private void OnApplicationQuit()
+    {
+        IsExiting = true;
+    }
+
+    private void OnDestroy()
+    {
+        //If exiting we return.
+        if (IsExiting)
+            return;
+
+        //We make sure that the part has the state "Placed" otherwise we return.
+        if (Part.CurrentState != StateType.Placed)
+            return;
+
+        Detach();
+    }
+
+    private void OnDestroyedPart(PartBehaviour part)
+    {
+        if (part != Part)
+            return;
+
+        if (!Part.CheckStability())
+            Detach();
+    }
+
+    private void Detach()
+    {
+        //We get all the renderers in the children.
+        List<Renderer> Renderers = GetComponentsInChildren<Renderer>(true).ToList();
+
+        for (int i = 0; i < Renderers.Count; i++)
+        {
+            if (Renderers[i].gameObject == null || !Renderers[i].gameObject.activeSelf)
+                return;
+
+            if (AddDynamicRigidbody)
+            {
+                Renderers[i].gameObject.AddRigibody(true, false, 1f);
+
+                Renderers[i].gameObject.GetComponent<Rigidbody>().maxDepenetrationVelocity = MaxDepenetrationVelocity;
+            }
+
+            if (AddDynamicBoxCollider)
+                Renderers[i].gameObject.AddComponent<BoxCollider>();
+
+            //Detach to parent.
+            Renderers[i].transform.parent = null;
+
+            //Destroy according life time.
+            Destroy(Renderers[i].gameObject, LifeTime);
+        }
+    }
+
+    #endregion
+}
